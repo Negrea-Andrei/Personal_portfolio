bd0f64695fce49a018c71a0c80cefad3c9645130
Negrea-Andrei <negrea97@yahoo.com>
2023-09-23 19:26:21 +0300

    Add animation to the picture and description zone

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import "./stylesheet.css";
-import printMe from "./test.js";
 
-console.log("hello!");
+function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+  
+  
+  function startAnimationWhenVisible() {
+    const picture = document.querySelector('.my-photo');
+    const text = document.querySelector('.about_me')
 
-printMe();
+    if(isElementInViewport(text)) {
+        text.style.animationPlayState = 'running'
+        picture.style.animationPlayState = 'running';
+    }
+  }
+
+window.addEventListener('scroll', startAnimationWhenVisible);
--- src/test.js
+++ src/test.js
@@ -1,3 +1,23 @@
-export default function printMe() {
-    console.log('I get called from print.js!');
+
+function isElementInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+
+function startAnimationWhenVisible() {
+  const picture = document.querySelector('.my-photo');
+  const text = document.querySelector('.about_me')
+
+  if (isElementInViewport(picture)) {
+    picture.style.animationPlayState = 'running';
+    text.style.animationPlayState = 'running'      
   }
+}
+
+export default startAnimationWhenVisible
