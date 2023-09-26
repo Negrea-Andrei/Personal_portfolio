dd05b2947ffe247e2e972c7af4e7bee9509ebd52
Negrea-Andrei <negrea97@yahoo.com>
2023-09-26 20:13:06 +0300

    Fixed height of the description of the section when the phone is sideways

--- src/index.js
+++ src/index.js
@@ -22,11 +22,9 @@ document.addEventListener("DOMContentLoaded", function () {
 function isElementInViewport(el) {
   const rect = el.getBoundingClientRect();
   return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
+    rect.top >= 0 ||
     rect.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      (window.innerHeight || document.documentElement.clientHeight)
   );
 }
 
